f59047c4c6ca3c8e4b788f81e895c20e6a07e185
rameshjgr <54370931+rameshjgr@users.noreply.github.com>
2019-08-21 23:00:44 +0530

    Add files via upload

---
+++ NumberToWordProcessor/NumToWordConverter.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace NumberToWordProcessor
+{
+    internal class NumToWordConverter
+    {
+        readonly string[] arrOneToNine = new string[] { "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine" };
+        readonly string[] arrTenToTwenty = new string[] { "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+                                                         "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty" };
+        readonly string[] arrThirtyToNinety = new string[] { "Thirty", "Fourty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+        readonly string strInvalidNumber = "Please enter valid number and try again....";
+
+        public string ConvertNumber(string inputNum)
+        {
+            string resultVal;
+            try
+            {
+                //First cehck if input is numeric (not alpha numeric)
+                if (long.TryParse(inputNum, out long result))
+                {
+                    if (inputNum == "0")
+                        return "Zero only";
+
+                    //Number is not Zero, it may start with Minus or may not, so check the condition and take decision
+                    if (inputNum.StartsWith("-"))
+                        resultVal = String.Format("Minus {0}", StartConversion(inputNum.Substring(1, inputNum.Length - 1)));
+                    else
+                        resultVal = StartConversion(inputNum);
+                }
+                else
+                    return strInvalidNumber;
+            }
+            catch
+            {
+                //Handle the exception, log it in appropriate location
+                return "Something went wrong, please try again!";
+            }
+            return string.IsNullOrEmpty(resultVal) ? strInvalidNumber : string.Concat(resultVal, " Only");
+        }
+        private String StartConversion(String inputNumber)
+        {
+            bool isCompleted = false;
+            string resultStr = string.Empty;
+            double number = (Convert.ToDouble(inputNumber));
+
+            if (number > 0)
+            {
+                int currentPosition = 0;
+                int numDigits = inputNumber.Length;
+                String currentPlace = string.Empty;
+                switch (numDigits)
+                {
+                    case 1:
+                        resultStr = ConvertOnes(inputNumber);
+                        isCompleted = true;
+                        break;
+                    case 2:
+                        resultStr = ConvertTens(inputNumber);
+                        isCompleted = true;
+                        break;
+                    case 3:
+                        currentPosition = (numDigits % 3) + 1;
+                        currentPlace = " Hundred ";
+                        break;
+                    case 4:
+                    case 5:
+                        currentPosition = (numDigits % 4) + 1;
+                        currentPlace = " Thousand ";
+                        break;
+                    case 6:
+                        currentPosition = (numDigits % 6) + 1;
+                        currentPlace = " Lac ";
+                        break;
+                    case 7:
+                    case 8:
+                    case 9:
+                        currentPosition = (numDigits % 7) + 1;
+                        currentPlace = " Million ";
+                        break;
+                    case 10:
+                    case 11:
+                    case 12:
+                        currentPosition = (numDigits % 10) + 1;
+                        currentPlace = " Billion ";
+                        break;
+                    default:
+                        isCompleted = true;
+                        break;
+                }
+                if (!isCompleted)
+                {
+                    if (inputNumber.Substring(0, currentPosition) != "0" && inputNumber.Substring(currentPosition) != "0")
+                        resultStr = StartConversion(inputNumber.Substring(0, currentPosition)) + currentPlace + StartConversion(inputNumber.Substring(currentPosition));
+                    else
+                        resultStr = StartConversion(inputNumber.Substring(0, currentPosition)) + StartConversion(inputNumber.Substring(currentPosition));
+                }
+
+                if (resultStr.Trim().Equals(currentPlace.Trim()))
+                    resultStr = string.Empty;
+            }
+            return resultStr.Trim();
+        }
+        private String ConvertTens(String inputNumber)
+        {
+            Int64 number = Convert.ToInt64(inputNumber);
+
+            if (number >= 10 && number <= 20)
+                return arrTenToTwenty[number - 10];
+            else if (number >= 30 && number <= 90 && number % 10 == 0)
+                return arrThirtyToNinety[(number / 10) - 3];
+            else if (number > 0)
+                return string.Concat(ConvertTens(inputNumber.Substring(0, 1) + "0"), " ", ConvertOnes(inputNumber.Substring(1)));
+
+            return string.Empty;
+        }
+        private String ConvertOnes(String inputNumber)
+        {
+            return arrOneToNine[Convert.ToInt64(inputNumber) - 1];
+        }
+    }
+}
---
+++ NumberToWordProcessor/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToWordProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press Y to execute sample test cases");
+            Console.WriteLine("Press anything else to continue with your own number");
+
+            string input = Console.ReadLine();
+
+            if (!string.IsNullOrEmpty(input) && !input.ToUpper().Equals("Y"))
+            {
+                Console.WriteLine("Okey, please enter the number that you want to convert into words..");
+                string inputNumber = Console.ReadLine();
+
+                NumToWordConverter objConverter = new NumToWordConverter();
+                Console.WriteLine(objConverter.ConvertNumber(inputNumber));
+            }
+            else
+                RunSampleTests();
+
+            Console.WriteLine("====================================================================================================");
+            Console.WriteLine("Press any key to exit....");
+            Console.ReadLine();
+        }
+
+        private static void RunSampleTests()
+        {
+            NumToWordConverter objConverter;
+            {
+                objConverter = new NumToWordConverter();
+                string result = string.Empty;
+                string strInputNumber = string.Empty;
+
+                Console.WriteLine("================================= Here is sample test cases result =================================");
+                Console.WriteLine();
+
+                Console.WriteLine(string.Format("Input : {0}             Output : {1}", strInputNumber, objConverter.ConvertNumber(string.Empty)));
+                Console.WriteLine(string.Format("Input : {0}             Output : {1}", null, objConverter.ConvertNumber(null)));
+                strInputNumber = "000";
+                Console.WriteLine(string.Format("Input : {0}          Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "-123";
+                Console.WriteLine(string.Format("Input : {0}         Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "0";
+                Console.WriteLine(string.Format("Input : {0}            Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "10";
+                Console.WriteLine(string.Format("Input : {0}           Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "14";
+                Console.WriteLine(string.Format("Input : {0}           Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "143";
+                Console.WriteLine(string.Format("Input : {0}          Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "200";
+                Console.WriteLine(string.Format("Input : {0}          Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "2000";
+                Console.WriteLine(string.Format("Input : {0}         Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "23456";
+                Console.WriteLine(string.Format("Input : {0}        Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "50000";
+                Console.WriteLine(string.Format("Input : {0}        Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "123456";
+                Console.WriteLine(string.Format("Input : {0}       Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "200000";
+                Console.WriteLine(string.Format("Input : {0}       Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "1234567";
+                Console.WriteLine(string.Format("Input : {0}      Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+                strInputNumber = "2000000";
+                Console.WriteLine(string.Format("Input : {0}      Output : {1}", strInputNumber, objConverter.ConvertNumber(strInputNumber)));
+
+            }
+        }
+    }
+}
